b7eccb421ae6c84b513bc487fe213b2f40dca096
kirin <kirin.chen1001@gmail.com>
2019-09-12 22:27:40 +0800

    init commit code

---
+++ Scenes/Demo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Demo : MonoBehaviour {
+    public Text pathText;
+    public Image image;
+
+    void Start() {
+        NativeToolkit.OnImagePicked += ImagePicked;
+    }
+
+    public void OnPickImagePress() {
+        NativeToolkit.PickImage();
+        pathText.text = "testing";
+    }
+
+    void ImagePicked(Texture2D myImg, string path) {
+        pathText.text = path;
+        Sprite s = Sprite.Create(myImg, new Rect(0, 0, myImg.width, myImg.height), Vector2.zero);
+        image.sprite = s;
+    }
+}
